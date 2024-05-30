5bad9fb5e73c449c1b9413b76b5f24804d07474e
bbroscheit <bernardo.broscheit@gmail.com>
2024-05-30 13:25:00 -0300

    agrega links en vista supervisor

--- client/components/CardTicketUser.js
+++ client/components/CardTicketUser.js
@@ -1,12 +1,20 @@
 import React, { useState, useRef}  from 'react'
+import { useRouter } from 'next/router';
 import style from '@/modules/cardTicketDeveloper.module.css'
 import { incialParaDesarrollador } from '@/functions/incialParaDesarrollador'
 import { devuelveInicial } from '@/functions/devuelveInicial'
 import { Tooltip } from '@mui/material'
 
 function CardTicketUser({id , state , subject , worker, key}) {
+  const router = useRouter();
   const [textPosition, setTextPosition] = useState(0);
   const intervalRef = useRef(null);
+
+  function idKeep(e) {
+    e.preventDefault();
+    const idSoporte = id;
+    localStorage.setItem("idSoporte", JSON.stringify(idSoporte));
+  }
 
   function handleMouseEnter() {
     intervalRef.current = setInterval(() => {
@@ -20,7 +28,7 @@ function CardTicketUser({id , state , subject , worker, key}) {
   }
 
   return (
-    <div className={style.container} key={key}>
+    <div className={style.container} key={key} onClick={(e) => { idKeep(e); router.push(`/soportes/[id]`, `/soportes/${id}`) }}>
         <div className={style.firstLine}>
             <h4 >T {id}</h4>
             <div className={style.divEstados}>
